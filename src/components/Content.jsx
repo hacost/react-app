fc5e76746a33b1bbd914c22203a802cbc6ebe5ca
Héctor Acosta <hacost@hotmail.com>
2021-02-20 21:44:51 -0600

    update scss common

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import '../assets/styles/components/Content.scss';
-import Link from '@material-ui/core/Link';
 
 const Content = () => (
   <section className='item content'>
     <h2>Content component </h2>
-    <Link href='#' variant='body2'>
-      Forgot password?
-    </Link>
   </section>
 );
 

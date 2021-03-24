d3e03c5eafda3137b6b34424754e817945692123
Héctor Acosta <hacost@hotmail.com>
2021-03-23 19:52:41 -0600

    show products in DataGrid

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import '../assets/styles/containers/Home.scss';
+import { DataGrid } from '@material-ui/data-grid';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'category', headerName: 'Category', width: 150 },
+  { field: 'name', headerName: 'Description', width: 400 },
+  {
+    field: 'price',
+    headerName: 'Price',
+    type: 'number',
+    width: 90,
+  },
+];
 
 const Home = (props) => {
   const { myList, products } = props;
@@ -11,6 +24,12 @@ const Home = (props) => {
   return (
     <section className='item content'>
       <>
+
+        <Categories title='Lista de productos'>
+          <div style={{ height: 400, width: '100%' }}>
+            <DataGrid rows={products} columns={columns} pageSize={5} checkboxSelection />
+          </div>
+        </Categories>
 
         {myList.length > 0 && (
           <Categories title='My sale products'>
